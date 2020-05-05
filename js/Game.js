42e50a28a78f3defa01e542d82e3398e89697757
Pratham-mishra <p3901pratham@gmail.com>
2020-05-05 15:30:25 +0530

    hi

--- js/Game.js
+++ js/Game.js
@@ -5,6 +5,7 @@ class Game {
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
        gameState = data.val();
+       console.log(gameState);
     })
    
   }
@@ -19,8 +20,35 @@ class Game {
     if(gameState === 0){
       player = new Player();
       player.getCount();
+      console.log(player)
       form = new Form()
       form.display();
     }
   }
+
+  play(){
+     form.hide();
+     text("Game Start",120,100);
+     Player.getPlayerInfo();
+
+      if(allPlayers !== undefined){
+        var displayPos = 130;
+        for(var plr in allPlayers){
+          if(plr === "player"+ player.index){
+              fill("red");
+
+          }
+          else{
+            fill("black");
+          }
+          displayPos+=20;
+          text(allPlayers[plr].name+" : "+ allPlayers[plr].distance,120,displayPos);
+        }
+        
+      }
+      if (keyIsDown(UP_ARROW)&& player.index!== null){
+        player.distance+=60;
+        player.update();
+   }
+    }
 }
--- js/Player.js
+++ js/Player.js
@@ -1,5 +1,9 @@
 class Player {
-  constructor(){}
+  constructor(){
+    this.distance = 0;
+    this.name = null;
+    this.index = null;
+  }
 
   getCount(){
     var playerCountRef = database.ref('playerCount');
@@ -14,10 +18,19 @@ class Player {
     });
   }
 
-  update(name){
-    var playerIndex = "player" + playerCount;
+  update(){
+    var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).set({
-      name:name
+      name:this.name,
+      distance : this.distance
     });
+
+  }
+
+   static getPlayerInfo(){
+    var allPlayerRef = database.ref('players');
+    allPlayerRef.on("value",function(data){
+      allPlayers = data.val();
+    })
   }
 }
